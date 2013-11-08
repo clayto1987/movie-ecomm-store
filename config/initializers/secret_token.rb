a4d26bd5fa5ad58218b41a12b3c32534c20700c6
Clayton Wallace <cwallace@academic.rrc.ca>
2013-11-08 16:37:07 -0600

    First Commit for ecomm project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieEcommStore::Application.config.secret_token = 'bfc9e9b65a2198dada3d5b85c195a44b1bcb49036d2a9f06148eb56b908a1c45834407bac02e684d07cc3d1123852bd4f6e400cf828a781bda2a7c3424dc4406'
