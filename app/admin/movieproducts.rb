10766d70a147a2efe44aae240a0794879d66f93f
Clayton Wallace <cwallace@academic.rrc.ca>
2013-11-15 09:36:46 -0600

    modified admin dashboard to display thumbnail images

--- app/admin/movieproducts.rb
+++ app/admin/movieproducts.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Movieproduct do
-  
+  batch_action :flag do |selection|
+    Post.find(selection).each { |p| p.flag! }
+    redirect_to collection_path, :notice => "Posts flagged!"
+  end
+
+  index do
+    selectable_column
+    column 'Image' do |image|
+      image_tag image.image_path, :size => '30x40'
+    end
+    column :product_type
+    column :description
+    column :price
+    column :stock_quantity
+    default_actions
+  end
 end
