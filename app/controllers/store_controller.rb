a22bee96b16261fe1041230a0a5fb60b854a809d
Clayton Wallace <cwallace@academic.rrc.ca>
2013-11-22 08:53:34 -0600

    fixed search issue, change select box to use select tag instead of collection select

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,9 +9,8 @@ class StoreController < ApplicationController
   end
 
   def search_results
-    if params.has_key?(:keywords) && params.has_key?(:category)
       keywords = params[:keywords]
-      product_category = params[:category][:filter]
+      product_category = params[:category]
 
       #possible problem with paginating because you can only access search results with post
       if product_category.empty?
@@ -19,8 +18,5 @@ class StoreController < ApplicationController
       else
         @movieproduct_results = Movieproduct.joins(:movie).where("movieproducts.category_id = #{product_category}").where(["movies.title LIKE ? OR movieproducts.description LIKE ?", "%#{keywords}%", "%#{keywords}%"]).order('movies.title').page(params[:page]).per(15)
       end
-    else
-      redirect_to root_path
-    end
   end
 end
