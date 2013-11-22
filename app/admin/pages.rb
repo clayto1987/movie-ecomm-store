df798617f362fb3606dd07759f7f5e229ebe539b
Clayton Wallace <cwallace@academic.rrc.ca>
2013-11-22 12:48:56 -0600

    started to add styling to active admin

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Page do
-  
+  form do |f|
+    f.inputs "Details" do
+      f.input :title
+      f.input :permalink
+      f.input :content, :input_html => {:class => 'static_textarea'}
+    end
+    f.actions
+  end
 end
