e334833b7c050a559dbac362de79974b6a46ee4c
Clayton Wallace <cwallace@academic.rrc.ca>
2013-11-21 18:05:40 -0600

    added search by category functionality, search is now complete - check with kyle about pagination on search results

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -10,6 +10,13 @@ class StoreController < ApplicationController
 
   def search_results
     keywords = params[:keywords]
-    @movieproduct_results = Movieproduct.joins(:movie).where(["movies.title LIKE ? OR movieproducts.description LIKE ?", "%#{keywords}%", "%#{keywords}%"]).order('movies.title').page(params[:page]).per(15)
+    product_category = params[:category][:filter]
+
+    #possible problem with paginating because you can only access search results with post
+    if product_category.empty?
+      @movieproduct_results = Movieproduct.joins(:movie).where(["movies.title LIKE ? OR movieproducts.description LIKE ?", "%#{keywords}%", "%#{keywords}%"]).order('movies.title').page(params[:page]).per(15)
+    else
+      @movieproduct_results = Movieproduct.joins(:movie).where("movieproducts.category_id = #{product_category}").where(["movies.title LIKE ? OR movieproducts.description LIKE ?", "%#{keywords}%", "%#{keywords}%"]).order('movies.title').page(params[:page]).per(15)
+    end
   end
 end
